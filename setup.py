85c68a442d530ce9f0a887e5f9e60858746be4c9
hmallen <allenhm@gmail.com>
2021-01-13 00:15:43 -0600

    Added core files and setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="loghandler",
+    version="0.0.1",
+    author="Hunter Allen",
+    author_email="allenhm@gmail.com",
+    description="Logging setup with stream and file handlers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hmallen/loghandler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
